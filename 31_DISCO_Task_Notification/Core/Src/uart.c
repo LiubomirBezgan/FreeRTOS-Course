33eb7e72c7e44e38e4fd16dea8557a3fd0c970f3
LiubomirBezgan <bezganliubomir@gmail.com>
2023-06-18 17:10:41 +0200

    31. Task notification
    STM32F411E-DISCO
    Task notification: ulTaskNotifyTake(), vTaskNotifyGiveFromISR().

---
+++ 31_DISCO_Task_Notification/Core/Src/uart.c
@@ -0,0 +1,99 @@
+/*
+ * uart.c
+ *
+ *  Created on: May 31, 2023
+ *      Author: <SKG> POTOP
+ */
+
+#include "uart.h"
+
+//UART_HandleTypeDef huart1;
+//
+///**
+//  * @brief USART1 Transmission Initialization Function
+//  * @param None
+//  * @retval None
+//  */
+//void USART1_UART_Tx_Init(void)
+//{
+//
+//  /* USER CODE BEGIN USART1_Init 0 */
+//
+//  /* USER CODE END USART1_Init 0 */
+//
+//  /* USER CODE BEGIN USART1_Init 1 */
+//
+//  /* USER CODE END USART1_Init 1 */
+//  huart1.Instance = USART1;
+//  huart1.Init.BaudRate = 115200;
+//  huart1.Init.WordLength = UART_WORDLENGTH_8B;
+//  huart1.Init.StopBits = UART_STOPBITS_1;
+//  huart1.Init.Parity = UART_PARITY_NONE;
+//  huart1.Init.Mode = UART_MODE_TX;
+//  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+//  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+//  if (HAL_UART_Init(&huart1) != HAL_OK)
+//  {
+////    Error_Handler();
+//  }
+//  /* USER CODE BEGIN USART1_Init 2 */
+//
+//  /* USER CODE END USART1_Init 2 */
+//
+//}
+//
+///**
+//  * @brief USART1 Receiving Initialization Function
+//  * @param None
+//  * @retval None
+//  */
+//void USART1_UART_Rx_Init(void)
+//{
+//
+//  /* USER CODE BEGIN USART1_Init 0 */
+//
+//  /* USER CODE END USART1_Init 0 */
+//
+//  /* USER CODE BEGIN USART1_Init 1 */
+//
+//  /* USER CODE END USART1_Init 1 */
+//  huart1.Instance = USART1;
+//  huart1.Init.BaudRate = 115200;
+//  huart1.Init.WordLength = UART_WORDLENGTH_8B;
+//  huart1.Init.StopBits = UART_STOPBITS_1;
+//  huart1.Init.Parity = UART_PARITY_NONE;
+//  huart1.Init.Mode = UART_MODE_RX;
+//  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+//  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+//  if (HAL_UART_Init(&huart1) != HAL_OK)
+//  {
+////    Error_Handler();
+//  }
+//  /* USER CODE BEGIN USART1_Init 2 */
+//
+//  /* USER CODE END USART1_Init 2 */
+//
+//}
+
+/**
+  * @brief  USART1 writing function
+  * @param  ch describes a sending character
+  * @retval ch describes the sending character
+  */
+int uart1_write(int ch)
+{
+	while(!(USART1->SR & 0x0080)) {}
+	USART1->DR = (ch & 0xFF);
+	return ch;
+}
+
+/**
+  * @brief  Sending function which use USART1 instead of a console
+  * @param  ch describes a sending character
+  * @retval ch describes the sending character
+  */
+int __io_putchar(int ch)
+{
+	uart1_write(ch);
+	return ch;
+}
